844f0b1d7c637f1c1f1692bb525a228c4d30ce08
VH1974i <ramirezvh@yahoo.es>
2026-05-21 21:59:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,8 @@ elif opcion == "Ejercicio 2":
                st.error(f"⚠️ La actividad '{nombre}' ya existe en la lista. Elige un nombre diferente.")
            else:
                st.session_state.actividades.append(registro)
-               tabla=pd.DataFrame(st.session_state.actividades)
-               event = st.dataframe(tabla)
+               #tabla=pd.DataFrame(st.session_state.actividades)
+               #event = st.dataframe(tabla)
    
 elif opcion == "Ejercicio 3":
    st.write("¡Bienvenido al menú Ejercicio 3!")
