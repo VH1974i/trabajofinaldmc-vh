d6258586e75153a6909f6777bad3d059c841245b
VH1974i <ramirezvh@yahoo.es>
2026-05-21 22:24:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -66,6 +66,7 @@ elif opcion == "Ejercicio 2":
                st.error(f"⚠️ La actividad '{nombre}' ya existe en la lista. Elige un nombre diferente.")
            else:
                st.session_state.actividades.append(registro)
+               st.success(f"¡Registro agregado satisfactoriamente!")
 
    if st.button("Limpiar lista!"):
        st.session_state.actividades = []
