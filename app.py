79c3c6be2ded88269a781eed5c21d083af449bb9
VH1974i <ramirezvh@yahoo.es>
2026-05-23 16:21:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ elif opcion == "Ejercicio 2":
                                             for i, actividad in enumerate(st.session_state.actividades)
                                             if i not in indices_seleccionados ]
            st.rerun()
-           st.info("Las actividades fueron evaluadas!")
+           st.success("Las actividades fueron evaluadas!")
 
 elif opcion == "Ejercicio 3":
    st.write("¡Bienvenido al menú Ejercicio 3!")
